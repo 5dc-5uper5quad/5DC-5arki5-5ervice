2e82e589809a02df32c995c715b63151aaaa56bd
Sarkis-S <Sarkis.Sos19@gmail.com>
2019-06-12 22:23:34 -0700

    This commit has a working 10M seeder and a working GET method.

--- database/postgresDB/index.js
+++ database/postgresDB/index.js
@@ -1,10 +1,62 @@
-const pg = require('pg');
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'sarkissos',
+  host: 'localhost',
+  database: 'reviews',
+  password: '1919',
+  port: 5432,
+});
 
-// Need schema
+pool.connect((err) => {
+  if (err) {
+    console.log('Connection error', err.stack);
+  } else {
+    console.log('Connected to database');
+  }
+});
 
-// save method
 
-// getReviews method
+/***********************************************/
+// This route should send back the reviews for a game by the game ID
+const getGameId = (req, res) => {
+  const id = parseInt(req.params.gameid);
+  pool.query('SELECT * FROM reviews WHERE game_id = $1', [id], (err, results) => {
+    console.log('RESULTS HERE:', results);
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(results.rows);
+  });
+}
 
+/***********************************************/
+// This route should post new review
+const createReview = (req, res) => {
+  // console.log('REQ', req);
+  // pool.query('INSERT INTO reviews (');
+}
 
-// Records should be representative of actual data. The use of libraries and tools like Faker, Lorem Ipsum (text) and Lorem Flickr (images) is recommended.
+/***********************************************/
+// This route should update (PUT) the post with submitted edits
+const updateUser = (request, response) => {
+
+}
+
+/***********************************************/
+// need DELETE request which deletes an item
+
+const deleteUser = (request, response) => {
+  
+}
+
+const fileLoader = (table, filepath, callback) => {
+  pool.query(`COPY ${table} FROM '${filepath}';`, (err, resp) => {
+    callback(err, resp);
+  });
+}
+
+module.exports = {
+  getGameId,
+  pool,
+  fileLoader
+}
